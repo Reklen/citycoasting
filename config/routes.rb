27ce515a52594746568d437832dca48f3b715278
James O'Dwyer <j.odwyer320@gmail.com>
2014-05-26 16:05:26 -0400

    Updated home page with stamps. added symbols

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :stamps
 
   devise_for :users
-  root "pages#home"
+  root "stamps#index"
   get "about" => "pages#about" # creates about_path
   
   # The priority is based upon order of creation: first created -> highest priority.

d280183d0301b23a74a06a08ffc4340ee866f265
James O'Dwyer <j.odwyer320@gmail.com>
2014-05-28 23:24:14 -0400

    Added pagination

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -4,7 +4,7 @@ class StampsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @stamps = Stamp.all.order("created_at DESC")
+    @stamps = Stamp.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 15)
   end
 
 

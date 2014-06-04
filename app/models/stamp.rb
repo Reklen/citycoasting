e9d9bd35df93d5fe290446d4c4c95b1732c1356f
James O'Dwyer <j.odwyer320@gmail.com>
2014-06-03 22:14:33 -0400

    Adjusted show image

--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -1,7 +1,7 @@
 class Stamp < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :large => "900x900<", :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :large => "700x700#", :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	validates :description, presence: true
 	validates :image, presence: true

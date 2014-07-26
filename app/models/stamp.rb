9fc67b2d1161e8913acae7aff15c916010440ae4
James O'Dwyer <j.odwyer320@gmail.com>
2014-07-25 23:05:47 -0400

    Adjusted to large image size for time being

--- app/models/stamp.rb
+++ app/models/stamp.rb
@@ -2,7 +2,7 @@ class Stamp < ActiveRecord::Base
 	belongs_to :user
 
 
-	has_attached_file :image, :styles => { :large => "600x400#", :rendered => "500x300#",:medium => "300x300#",:thumber => "300x200#", :thumb => "100x100#" }
+	has_attached_file :image, :styles => { :large => "600x400#", :rendered => "500x300#",:medium => "300x300#", :thumb => "100x100#" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	validates :description, presence: true
 	validates :image, presence: true

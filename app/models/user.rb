504580be2f99ad6a9191c0a524637ed101e4ac5a
James O'Dwyer <j.odwyer320@gmail.com>
2014-09-01 14:08:27 -0400

    removed previous omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,7 @@ class User < ActiveRecord::Base
   has_many :evaluations, class_name: "RSEvaluation", as: :source
   has_reputation :votes, source: {reputation: :votes, of: :stamps}, aggregated_by: :sum
   
-#FB LOGIN
-  def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name
-      user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.save!
-    end
-  end
+
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     if user

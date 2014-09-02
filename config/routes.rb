a4d8d3976919676ba7ec7d9155cd5cd9015f98e8
James O'Dwyer <j.odwyer320@gmail.com>
2014-09-02 02:35:38 -0400

    fb login

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,9 @@ Rails.application.routes.draw do
 
   get 'profiles/:id', to: 'profiles#show', as: 'profile'
 
- get 'auth/:provider/callback' => 'sessions#create'
- get 'auth/failure' => redirect('/')
- get 'signout' => 'sessions#destroy', as: 'signout'
-  
+match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+match 'auth/failure', to: redirect('/'), via: [:get, :post]
+match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

606a2dd9597b6d18b0b94e928a5b8041a3eba78c
James O'Dwyer <j.odwyer320@gmail.com>
2014-09-02 09:33:19 -0400

    Adjusted back omniauth settings

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,1 @@
 OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1505096556404291', '8997c9ef91ee7902b955f4efbf5c4d64'
-end

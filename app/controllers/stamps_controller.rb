06e0aa971ab268b2bde225c957b793f8df039f10
James O'Dwyer <j.odwyer320@gmail.com>
2014-10-11 00:42:47 -0400

    Adjusting filter

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -5,9 +5,9 @@ class StampsController < ApplicationController
   
   def index
     if params[:category_id]
-      @stamps = Stamp.where(:rank != nil, category_id: params[:category_id]).all.order("rank ASC").paginate(:page => params[:page], :per_page => 10)
+      @stamps = Stamp.where(:rank > 0, category_id: params[:category_id]).all.order("rank ASC").paginate(:page => params[:page], :per_page => 10)
     else
-      @stamps = Stamp.where( :rank != nil).order("rank ASC").all.paginate(:page => params[:page], :per_page => 10)
+      @stamps = Stamp.where( :rank > 0).order("rank ASC").all.paginate(:page => params[:page], :per_page => 10)
     end
 
     respond_to do |format|

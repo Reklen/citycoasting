d98b9b93c066481226f32706735ab5d6dd155820
James O'Dwyer <j.odwyer320@gmail.com>
2014-10-11 01:11:39 -0400

    Adjusted category filter for home page

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -5,7 +5,8 @@ class StampsController < ApplicationController
   
   def index
     if params[:category_id]
-      @stamps = Stamp.where("rank is not null", category_id: params[:category_id]).all.order("rank ASC").paginate(:page => params[:page], :per_page => 10)
+      @stamps = Stamp.where(category_id: params[:category_id])
+      @stamps = @stamps.where("rank is not null").all.order("rank ASC").paginate(:page => params[:page], :per_page => 10)
     else
       @stamps = Stamp.where("rank is not null").order("rank ASC").all.paginate(:page => params[:page], :per_page => 10)
     end

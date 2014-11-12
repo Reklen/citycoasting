5b8e61feb7a1bb80b2599a5253aed1694416a9d0
James O'Dwyer <j.odwyer320@gmail.com>
2014-11-12 00:37:48 -0500

    Changed city show pg to ascending order

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -10,9 +10,9 @@ class CitiesController < ApplicationController
   	gon.city_instance_id = @city.id
   	
   	if params[:category_id]
-  		@stamps = Stamp.all.where(city_id: @city.id, category_id: params[:category_id]).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+  		@stamps = Stamp.all.where(city_id: @city.id, category_id: params[:category_id]).order("created_at ASC").paginate(:page => params[:page], :per_page => 10)
   	else
-      @stamps = Stamp.all.where(city_id: @city.id).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+      @stamps = Stamp.all.where(city_id: @city.id).order("created_at ASC").paginate(:page => params[:page], :per_page => 10)
     end
 
   end

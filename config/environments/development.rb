dd94eaeab9475feac6ff3352cc5b11cf1bba7d8b
James O'Dwyer <j.odwyer320@gmail.com>
2015-02-11 23:09:13 -0600

    formatted profile img

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,9 +38,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => Rails.application.secrets.AWS_BUCKET,
-    :access_key_id => Rails.application.secrets.AWS_ACCESS_KEY_ID,
-    :secret_access_key => Rails.application.secrets.AWS_SECRET_ACCESS_KEY
+    :bucket => Rails.application.secret_dev.AWS_BUCKET,
+    :access_key_id => Rails.application.secret_dev.AWS_ACCESS_KEY_ID,
+    :secret_access_key => Rails.application.secret_dev.AWS_SECRET_ACCESS_KEY
     }
   }
 

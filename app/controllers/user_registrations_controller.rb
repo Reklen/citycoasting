9d393fb9dee2f3796c520e30070d83c31339a344
James O'Dwyer <j.odwyer320@gmail.com>
2015-02-20 20:49:56 -0600

    Testing mailer

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -2,7 +2,7 @@ class UserRegistrationsController < Devise::RegistrationsController
 
 	def create
 		super
-		UserMailer.new_user_geet(@resource).deliver
+		UserMailer.new_user_geet(resource).deliver
 		
 	end
 

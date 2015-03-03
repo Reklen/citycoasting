8c2e3933a65b29da78c6d3d7e1e88ca1cb25cb6d
James O'Dwyer <j.odwyer320@gmail.com>
2015-03-02 20:43:56 -0500

    changed to instance var

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,8 @@ class UserMailer < ActionMailer::Base
 
   def new_user_geet(new_user)
   	recipients = ['j.odwyer320@gmail.com', 'talthoren@gmail.com']
-  	user = new_user
-  	mail(to: recipients, subject: "New user, #{user.username}, has signed up for CityCoasting")
+  	@user = new_user
+  	mail(to: recipients, subject: "New user, #{@user.username}, has signed up for CityCoasting")
   end
 
 end

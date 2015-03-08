fea2f00e05f9f7f5f68d32f28b0bf83a73a81d8a
James O'Dwyer <j.odwyer320@gmail.com>
2015-03-08 19:20:07 -0400

    Implemented fix to allow users to update profile without required password

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -4,7 +4,12 @@ class UserRegistrationsController < Devise::RegistrationsController
 		super do
 			UserMailer.new_user_geet(resource).deliver
 		end
-		
 	end
+	
+	protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
 end

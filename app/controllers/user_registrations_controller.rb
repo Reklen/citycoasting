72fcec14e4945e76e28966362f552e9098ddecc6
James O'Dwyer <j.odwyer320@gmail.com>
2015-03-08 19:32:45 -0400

    Ripped out password requirement for all users(email or fb)

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -5,16 +5,11 @@ class UserRegistrationsController < Devise::RegistrationsController
 			UserMailer.new_user_geet(resource).deliver
 		end
 	end
-	
+
 	protected
 
   def update_resource(resource, params)
-    if current_user.provider == "facebook"
-      params.delete("current_password")
-      resource.update_without_password(params)
-    else
-      resource.update_with_password(params)
-    end
+    resource.update_without_password(params)
   end
 
 

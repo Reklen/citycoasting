2556251af9a4d0d8a33f0ee9443ba85b0a7473cb
James O'Dwyer <j.odwyer320@gmail.com>
2015-03-14 18:00:25 -0400

    Additional design adjustments to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,6 @@
 class HomeController < ApplicationController
   def index
-    
-      @stamps = Stamp.first(2)
-   
-    
+    @stamps = Stamp.where("rank is not null").order("rank ASC").all.paginate(:page => params[:page], :per_page => 10)
     respond_to do |format|
       format.html { render :layout => false, flash: { myModal: true } } 
       format.js
